f820b0e30f23f275e1d0c64661f8e1c2ccab0b74
Saemi Hong <sstyle940104@gmail.com>
2022-04-06 23:25:10 +0900

    init

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+const {addUser, removeUser, getUser, getUsersInRoom} = require("./users");
+
+const PORT = process.env.PORT || 5000;
+
+const router = require('./router');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server, {cors: {
+    origin: "http://localhost:3000",
+    credentials: true
+  }}); //making socket io server working
+
+  io.on('connection', (socket) => {
+    socket.emit('message', {user: 'admin', text: "환영합니다~ 회원 가입을 위해 닉네임을 적어주세요"});
+    socket.on('')
+
+    socket.on('disconnect', () =>{
+        const user = removeUser(socket.id);
+        io.to(user.room).emit('message', {user: 'admin', text: `${user.name}님 다음에 또 봐요~`});
+
+        console.log("user has left!");
+    });
+  })
+  app.use(router);
+  server.listen(PORT, () => console.log(`server has started on port ${PORT}`));
---
+++ server/users.js
@@ -0,0 +1,34 @@
+const users = [];
+
+const addUser = ({id, name, room, email}) =>{
+    name = name.trim().toLowerCase();
+    email = email
+
+    const existingUser = users.find((user) => user.room === room && user.name === name);
+
+    if(existingUser){
+        return {error: "Username already taken"};
+    }
+
+    const user = {id, name, room, email};
+    users.push(user);
+    return {user}
+}
+
+const removeUser = (id) =>{
+    const index = users.findIndex((user) => user.id === id);
+
+    if(index !== -1){ //-1 : false, 1: true
+        return users.splice(index, 1)[0]
+    }
+}
+
+const getUser = (id) => {
+    return users.find(user => user.id === id)
+}
+
+const getUsersInRoom = (room) =>{
+    return users.filter(user => user.room === room);
+}
+
+module.exports = {addUser, removeUser, getUser, getUsersInRoom};
