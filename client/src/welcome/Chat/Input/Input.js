d4cc16ba34c8f5bf1ec284449bf730c94ab60483
Saemi Hong <sstyle940104@gmail.com>
2022-04-07 22:53:00 +0900

    complete input

--- client/src/welcome/Chat/Input/Input.js
+++ client/src/welcome/Chat/Input/Input.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import styled from "styled-components"
+import {SendOutlined} from "@ant-design/icons"
 
 const StyledInput = styled.input`
     width: 100%;
     height: 10vh;
+    line-height: 10vh;
     border: none;
     border-top: 1px solid #CDCDCD;
     color: #373C47;
@@ -19,10 +21,35 @@ const StyledInput = styled.input`
     }
 `;
 
+const StyledSendButton = styled.button`
+    position: absolute;
+    bottom: 2.5%;
+    right:0;
+    z-index:9;
+    color: #373C47;
+    background-color: transparent;
+    border: none;
+    font-size: 3rem;
+    transform: rotate(-35deg);
+    &:hover{
+        color: #fff;
+    }
+`;
+
+const SendMessage = () => {
+    return(
+    <StyledSendButton>
+        <SendOutlined />
+    </StyledSendButton>
+    )
+}
+
 function Input() {
+
   return (
     <div>
         <StyledInput placeholder='정보 입력 창'/>
+        <SendMessage />
     </div>
   )
 }
