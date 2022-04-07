0b06c30c2e88c017ddc6b8b6c9b78525d77ed6de
Saemi Hong <sstyle940104@gmail.com>
2022-04-07 23:21:18 +0900

    fixing socket error

--- client/src/welcome/Chat/Messages/Messages.js
+++ client/src/welcome/Chat/Messages/Messages.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import io from 'socket.io-client';
+
+
+let socket;
 
 function Messages() {
+const ENDPOINT = 'localhost:5000';
+const [message, setMessage] = useState("");
+const [messages, setMessages] = useState("");
+
+
+useEffect(() => {
+  socket = io(ENDPOINT);
+
+  socket.on('message', (message) => {
+    setMessage(message) //adding Msg from admin or any msgs
+  })
+}, [message])
   return (
-    <div>Messages</div>
+    <div style={{color: "#fff"}}>{message}</div>
   )
 }
 
--- server/index.js
+++ server/index.js
@@ -10,17 +10,23 @@ const router = require('./router');
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server, {cors: {
-    origin: "http://localhost:3000",
+    origin: "http://localhost:3001",
     credentials: true
   }}); //making socket io server working
 
   io.on('connection', (socket) => {
-    socket.emit('message', {user: 'admin', text: "환영합니다~ 회원 가입을 위해 닉네임을 적어주세요"});
-    socket.on('')
+     
+    socket.on('join',  (callback) => {
+        console.log("join join");
+        if(error) return callback(error) //error msg is coming from addUser
+
+        socket.emit('message', {user: 'admin', text: "환영합니다~ 회원 가입을 위해 닉네임을 적어주세요"});
+        callback();
+    })
 
     socket.on('disconnect', () =>{
         const user = removeUser(socket.id);
-        io.to(user.room).emit('message', {user: 'admin', text: `${user.name}님 다음에 또 봐요~`});
+       // io.to(user.room).emit('message', {user: 'admin', text: `${user.name}님 다음에 또 봐요~`});
 
         console.log("user has left!");
     });
